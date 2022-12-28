11debbdb4edaab331ba69decb6430b83d30f14bf
Jose David <jose7312000@gmail.com>
2022-12-27 23:52:25 -0500

    Modificando migracion de producto y creando relacion entre el modelo de producto y el de ventas

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $table = "products";
+    protected $fillable = [ 'name', 'reference', 'weight', 'category', 'stock'];
+    public $timestamps = true;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
